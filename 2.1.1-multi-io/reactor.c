01d63d62bf8ed7861097748841813f46d2f4871b
DreamyBlueLunar <2010614119@qq.com>
2024-08-26 13:52:07 +0000

    update reactor.c

--- 2.1.1-multi-io/reactor.c
+++ 2.1.1-multi-io/reactor.c
@@ -1,5 +1,3 @@
-// implementation of reactor
-// reactor.c
 #include <string.h>
 #include <errno.h>
 #include <sys/socket.h>
@@ -8,21 +6,20 @@
 #include <unistd.h>
 #include <sys/epoll.h>
 
-#define BUFFER_LEN 1024
-#define DEBUG_LEVEL 0
-#define ADD 1
-#define MOD 0
+#define CONN_LEN 	4096
+#define BUFFER_LEN 	1024
+#define DEBUG_LEVEL 1
+#define MOD 		1
+#define ADD			0
 
 typedef int (*RCALLBACK)(int fd);
 
-// conn, fd, buffer, callback
 struct conn_item {
 	int fd;
 	
-	char rbuffer[BUFFER_LEN]; // read
+	char rbuffer[BUFFER_LEN];
 	int rlen;
-	
-	char wbuffer[BUFFER_LEN]; // write
+	char wbuffer[BUFFER_LEN];
 	int wlen;
 
 	union {
@@ -32,231 +29,195 @@ struct conn_item {
 	RCALLBACK send_callback;
 };
 
-struct conn_item conn_list[1024];
-struct epoll_event ev;
-
 int epfd = 0, sockfd = 0;
+struct conn_item connlist[CONN_LEN] = {0};
 
-/* sockfd triggers EPOLLIN -> accept_cb()
- * params:
- * fd: sockfd
- */
 int accept_cb(int fd);
-
-/* clientfd triggers EPOLLIN -> recv_cb()
- * params:
- * fd: clientfd
- */
 int recv_cb(int fd);
-
-/* clientfd triggers EPOLLOUT -> send_cb()
- * params:
- * fd: clientfd
- */
 int send_cb(int fd);
-
-/* set event
- * params:
- * fd: file describtor
- * op: operation
- * event: EPOLLIN / EPOLLOUT
- * return:
- * success -> return 1
- * add error -> return -1
- * mod error -> return 0
- */
 int set_event(int fd, int op, int event);
 
 /*
- * http server implementation
+ * set event
+ * params:
+ * fd: sockfd or clientfd
+ * op: operations, add or modify
+ * event: EPOLLIN or EPOLLOUT
+ * returns 1(succeeded) or -1(failed)
  */
-int main(void) {
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (-1 == sockfd) {
-		perror("create sockfd");
+int set_event(int fd, int op, int event) {
+	struct epoll_event ev;
+	if (op == ADD) {
+		ev.events = event;
+		ev.data.fd = fd;
+		if (-1 == epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev)) {
+			#if DEBUG_LEVEL >= 1
+				printf("failed to add event\n");
+			#endif
+
+			return -1;			
+		}
+	} else {
+		ev.events = event;
+		ev.data.fd = fd;
+		if (-1 == epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev)) {
+			#if DEBUG_LEVEL >= 1
+				printf("failed to modify event\n");
+			#endif
+
+			return -1;			
+		}
+	}
+
+	return 1;
+}
+
+/*
+ * accept's callback func
+ * params:
+ * fd: connfd
+ * returns clientfd or -1
+ */
+int accept_cb(int fd) {
+	struct sockaddr_in client_addr;
+	socklen_t len = sizeof client_addr;
+	int clientfd = accept(fd, (struct sockaddr*)&client_addr, &len);
+	if (-1 == clientfd) {
+		#if DEBUG_LEVEL >= 1
+			printf("failed to create clientfd\n");
+		#endif
+		
 		return -1;
 	}
+
+	connlist[clientfd].fd = clientfd;
+	memset(connlist[clientfd].rbuffer, 0, BUFFER_LEN);
+	connlist[clientfd].rlen = 0;
+	memset(connlist[clientfd].rbuffer, 0, BUFFER_LEN);	
+	connlist[clientfd].wlen = 0;
+	connlist[clientfd].recv_t.recv_callback = recv_cb;
+	connlist[clientfd].send_callback = send_cb;
+
+	set_event(clientfd, ADD, EPOLLIN);
 	
+	return clientfd;
+}
+
+
+/*
+ * recv's callback func
+ * params:
+ * fd: connfd
+ * returns cnt or -1
+ */
+int recv_cb(int fd) {
+	char *buffer = connlist[fd].rbuffer;
+	int idx = connlist[fd].rlen;
+	
+	int cnt = recv(fd, buffer + idx, BUFFER_LEN - idx, 0);
+	if (cnt == 0) {
+		#if DEBUG_LEVEL >= 1
+			printf("disconnect\n");
+		#endif
+		epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);		
+		close(fd);
+		
+		return -1;
+	}
+	connlist[fd].rlen += cnt;
+
+	memcpy(connlist[fd].wbuffer, connlist[fd].rbuffer, connlist[fd].rlen);
+	connlist[fd].wlen = connlist[fd].rlen;
+	connlist[fd].rlen -= connlist[fd].rlen;
+	
+	set_event(fd, MOD, EPOLLOUT);
+
+	
+	return cnt;
+}
+
+
+/*
+ * send's callback func
+ * params:
+ * fd: connfd
+ * returns cnt
+ */
+int send_cb(int fd) {
+	char *buffer = connlist[fd].wbuffer;
+	int idx = connlist[fd].wlen;
+	int cnt = send(fd, buffer, idx, 0);
+	set_event(fd, MOD, EPOLLIN);
+
+	return cnt;
+}
+
+
+int main(void) {
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (-1 == sockfd) {
+		#if DEBUG_LEVEL >= 1
+			printf("failed to create sockfd\n");
+		#endif
+		
+		return -1;
+	}
+
 	struct sockaddr_in server_addr;
-	memset(&server_addr, 0, sizeof(struct sockaddr_in));
-	
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(2048);
 
-	if (-1 == bind(sockfd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr))) {
-		perror("bind");
- 		return -1;
+	if (-1 == bind(sockfd, (struct sockaddr*)&server_addr, sizeof (struct sockaddr))) {
+		#if DEBUG_LEVEL >= 1
+			printf("failed to bind sockfd and server_addr\n");
+		#endif
+
+		return -1;
 	}
 
-	listen(sockfd, 128);
+	listen(sockfd, 10);
 
-	conn_list[sockfd].fd = sockfd;
-	conn_list[sockfd].recv_t.accept_callback = accept_cb;
+	connlist[sockfd].fd = sockfd;
+	connlist[sockfd].recv_t.accept_callback = accept_cb;
 
 	epfd = epoll_create(1);
 	if (-1 == epfd) {
-		perror("create epoll");
+		#if DEBUG_LEVEL >= 1
+			printf("failed to create epfd\n");
+		#endif
+
 		return -1;
 	}
-	
-	if (1 != set_event(sockfd, ADD, EPOLLIN)) {
-		perror("add sockfd");
-		return -1;
-	}
+
+	set_event(sockfd, ADD, EPOLLIN);
 
 	struct epoll_event events[1024] = {0};
-	memset(&events, 0, sizeof events);
-	
-	printf("this is http-server-test.\n");
+	memset(events, 0, sizeof events);
 
-	while (1) { // main loop
-		printf("http_server@test:/$ ");
-		fflush(stdout);
+	while (1) {
+//		printf("fuk\n");
 
-		int nready = epoll_wait(epfd, events, 1024, -1);
+		int nready = epoll_wait(epfd, events, 1024, -1);		
 		for (int i = 0; i < nready; i ++) {
 			int connfd = events[i].data.fd;
 			
-			if (events[i].events & EPOLLIN) { // if sockfd triggers EPOLLIN, 
-						//we call accept_cb() successfully here,cause we set the union's value correctly.
+			if (events[i].events & EPOLLIN) {
 
-				int count = conn_list[connfd].recv_t.recv_callback(connfd);
+				int count = connlist[connfd].recv_t.recv_callback(connfd);
+				#if DEBUG_LEVEL >= 2
+					printf("recv count: %d <-- buffer: %s\n", count, connlist[connfd].rbuffer);
+				#endif
+			} else if (events[i].events & EPOLLOUT) { 
+				#if DEBUG_LEVEL >= 2
+					printf("send --> buffer: %s\n",  connlist[connfd].wbuffer);
+				#endif
+				int count = connlist[connfd].send_callback(connfd);
 
-			} else if (events[i].events & EPOLLOUT) {
-
-				int count = conn_list[connfd].send_callback(connfd);
-			 
 			}
 		}
 	}
 
 	return 0;
 }
-
-
-int accept_cb(int fd) {
-	struct sockaddr_in client_addr;
-	socklen_t len = sizeof client_addr;
-
-	int clientfd = accept(fd, (struct sockaddr*)&client_addr, &len);
-	if (0 > clientfd) {
-		perror("accept");
-		return -1;
-	}
-	
-	if (1 != set_event(clientfd, ADD, EPOLLIN)) {
-		perror("add clientfd");
-		return -1;
-	}
-
-	conn_list[clientfd].fd = clientfd;
-	memset(conn_list[clientfd].rbuffer, 0, BUFFER_LEN);
-	memset(conn_list[clientfd].wbuffer, 0, BUFFER_LEN);
-	conn_list[clientfd].rlen = 0;
-	conn_list[clientfd].wlen = 0;
-	conn_list[clientfd].recv_t.recv_callback = recv_cb;
-	conn_list[clientfd].send_callback = send_cb;
-
-#if DEBUG_LEVEL 
-
-	printf("sockfd: %d, clientfd: %d\n", fd, clientfd);
-
-#else
-
-	printf("accept done\n");
-
-#endif
-
-	return clientfd;
-}
-
-
-int recv_cb(int fd) {
-	// recv_cb();
-	char *buffer = conn_list[fd].rbuffer;
-	int idx = conn_list[fd].rlen;
-	int cnt = recv(fd, buffer + idx, BUFFER_LEN - idx, 0); // if client calls close(), recv() returns 0.
-
-	if (0 == cnt) {
-		perror("client disconnection");
-		epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
-		close(fd);
-		return -1;
-	}
-	conn_list[fd].rlen += cnt;
-	// operations
-	memcpy(conn_list[fd].wbuffer, conn_list[fd].rbuffer, conn_list[fd].rlen);
-	conn_list[fd].wlen = conn_list[fd].rlen;
-
-	if (1 != set_event(fd, MOD, EPOLLOUT)) {
-		perror("modify clientfd EPOLLOUT");
-		return -1;
-	}
-
-#if DEBUG_LEVEL
-
-	printf("recv <-- sockfd: %d, clientfd: %d, cnt: %d, buffer: %s\n",
-			sockfd, fd, cnt, conn_list[fd].rbuffer);
-
-#else
-		
-	printf("recv done\n");
-
-#endif
-
-	return cnt;
-}
-
-
-int send_cb(int fd) {
-	int cnt = send(fd, conn_list[fd].wbuffer, conn_list[fd].wlen, 0);
-	if (-1 == cnt) {
-		perror("send");
-		return -1;
-	}
-	
-	if (1 != set_event(fd, MOD, EPOLLIN)) {
-		perror("modify clientfd EPOLLOUT");
-		return -1;
-	}
-
-
-#if DEBUG_LEVEL
-
-	printf("send --> sockfd: %d, clientfd: %d, cnt: %d, buffer: %s\n",
-			sockfd, fd, cnt, conn_list[fd].wbuffer);
-#else
-			
-	printf("send done\n");
-
-#endif	
-	
-	return cnt;
-}
-
-
-int set_event(int fd, int op, int event) {
-	if (op == 1) { // 1 add, 0 mod
-
-		ev.events = event;
-		ev.data.fd = fd;
-		
-		if (-1 == epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev) ) {
-			return -1;
-		}
-
-	} else {
-
-		ev.events = event;
-		ev.data.fd = fd;
-		
-		if (-1 == epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev) ) {
-			return 0;
-		}
-
-	}
-
-	return 1;
-}
-
